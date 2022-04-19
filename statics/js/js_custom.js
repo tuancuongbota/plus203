6c0014e314ecd313166810ebccc1cdb408575305
tuancuongbota <93958224+tuancuongbota@users.noreply.github.com>
2022-04-19 17:28:07 +0700

    cuongnt

--- statics/js/js_custom.js
+++ statics/js/js_custom.js
@@ -39,4 +39,28 @@ $(document).ready(function() {
 		$('.bota_header').toggleClass('active');
 		$(this).toggleClass('active');
 	});
+    $(window).on('load', function () {
+		AOS.refresh();
+	})
+	AOS.init({
+		startEvent: 'load',
+		easing: 'ease-out-back',
+		duration: 1500,
+		once: true
+	});
+    $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    $(".bota_block_technology_title").on("click", function () {
+        $(this).toggleClass("open");
+        $(this).next().slideToggle();
+    });
 });
